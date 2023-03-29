009959ebc716aecbf10d01fa8a49c01f458fa0d6
generalrowland <ethandale0916@gmail.com>
2023-03-29 13:56:56 -0500

    4

--- Methods_Rowland_Ethan/Program.cs
+++ Methods_Rowland_Ethan/Program.cs
@@ -6,7 +6,40 @@ namespace Methods_Rowland_Ethan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a whole number!");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another whole number!");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Multiply(x,y));
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            WelcomeUser(name);
+
+            Goodbye();
         }
+
+        static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        static void WelcomeUser(string username)
+        {
+            Console.WriteLine($"Hello, {username}! It is nice to meet you!");
+        }
+
+        /// <summary>
+        /// Pritns a goodbye message to the console
+        /// </summary>
+
+
+        static void Goodbye()
+        {
+            Console.WriteLine("Have a fantastic day!");
+        }
+
     }
 }
